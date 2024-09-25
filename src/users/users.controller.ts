99b20920c311da61dd56e5a24dc76e6800430465
f.staikos7 <f.staikos7@gmail.com>
2024-09-25 23:08:00 +0300

    Initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Post , Body, Get, Patch, Param, Query, Delete,  } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dt';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('auth')
+export class UsersController {
+    constructor(private usersService: UsersService){}
+    @Post('/signup')
+    createuser(@Body() body: CreateUserDto){
+        this.usersService.create(body.email, body.password);
+        console.log(body);
+        
+    }
+    @Get('/:id')
+    findUser(@Param('id') id: string){
+        return this.usersService.findOne(parseInt(id));
+
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string){
+        return this.usersService.find(email);
+
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string){
+        return this.usersService.remove(parseInt(id));
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto){
+        return this.usersService.update(parseInt(id), body);
+    }
+}
